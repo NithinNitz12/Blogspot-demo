c6b97e3c8a8615d63da91389ba700dc221c71039
Nithin <nithinraj10@hotmail.com>
2021-09-04 00:09:07 +0530

    Blog page

--- blog/myblog/post/views.py
+++ blog/myblog/post/views.py
@@ -1,16 +1,21 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Post
 def index(request):
     featured = Post.objects.filter(featured=True)
-    latest = Post.objects.droer_by('-timestamp') [0:3]
+    latest = Post.objects.order_by('-timestamp') [0:3]
     context = {
-        'object_list': featured
+        'object_list': featured,
         'latest': latest
     }
     return render(request, 'index.html', context)
 
 def blog(request):
-    return render(request,"blog.html", {})
+    post_list = Post.objects.all()
+    context = {
+        'post_list': post_list
+    }
+    return render(request,"blog.html", context)
 
 def post(request):
     return render(request,"post.html", {})
