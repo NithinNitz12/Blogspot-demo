3c66fd6389c4964953a7ac3d2c036966145ea3f7
Nithin <nithinraj10@hotmail.com>
2021-09-04 00:42:57 +0530

    Blog pagination

--- blog/myblog/post/views.py
+++ blog/myblog/post/views.py
@@ -12,8 +12,18 @@ def index(request):
 
 def blog(request):
     post_list = Post.objects.all()
+    paginator = Paginator(post_list, 4)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
     context = {
-        'post_list': post_list
+        'queryset': paginated_queryset,
+        'page_request_var': page_request_var
     }
     return render(request,"blog.html", context)
 
